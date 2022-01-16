a890b3c4b6f69df1564c6c2dd24e30916c7ae1d6
Ruslan Tibilashvili <ruslan.tibilashvili@gmail.com>
2022-01-16 19:22:06 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Array from "./array";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
-export default App;
+    constructor() {
+        super();
+        this.Array = {
+            out: 0
+        }
+        this.refOutput = React.createRef();
+    }
+
+    myFunction(value){
+        let currentValue = value;
+        let output = this.refOutput.current
+        this.setState({out: currentValue})
+
+        if(output.value === '0'){
+            output.value = '';
+        }
+        output.value += currentValue
+
+       if (output.value === 'Infinity') {
+            setTimeout(() => {
+                output.value = '0'
+            }, 1000)
+        }
+
+    }
+
+    myOperationFunction(value){
+        let output = this.refOutput.current;
+        if (value === "="){
+           output.value = eval(output.value);
+        }
+    }
+
+
+
+    render() {
+        return(
+            <div className="container">
+              <div className="output">
+               <input ref={this.refOutput} type= "text" defaultValue={this.Array.out}></input>
+              </div>
+                <div className="buttons">
+                    {Array.buttons.map(item =>
+                     <button onClick={()=> {this.myFunction(item.val)}}>
+                         {item.val}</button>)}
+                    {Array.operations.map(item =>
+                        <button onClick={()=> {this.myOperationFunction(item.val)}}>
+                            {item.val}</button>)}
+                </div>
+            </div>
+            )
+        }
+    }
+
+    export default App
